f94d892b43e8273c7274f10eac3b4409998c7cb8
15986776923 <1355818157@qq.com>
2022-10-14 22:54:32 +0800

    init project

---
+++ bilibili-api/src/main/java/com/Morgan/BiliBiliApp.java
@@ -0,0 +1,17 @@
+package com.Morgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author Morgan
+ * @create 2022-10-14-22:43
+ */
+@SpringBootApplication
+public class BiliBiliApp {
+    public static void main(String[] args) {
+        ApplicationContext app = SpringApplication.run(BiliBiliApp.class, args);
+
+    }
+}
